96fad4da567baa4dfc293cfde7339016f27eee10
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-16 16:09:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,9 @@
 class Owner
-  attr_accessor :name, :pets, :dog, :cat
-  attr_reader :species
+  attr_accessor :pets, :dog, :cat
+  attr_reader :species, :name
 
   @@all = []
-  
+
 
   def initialize(name)
     @species = species
